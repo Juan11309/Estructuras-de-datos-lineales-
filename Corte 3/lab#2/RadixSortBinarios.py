70e3206dd2912925ba15d6da39c3313c581dd5ec
Valerie63-a <valeriegutizam@gmail.com>
2026-05-09 22:35:39 -0500

    Rename variable and refactor radix_sort function

--- Corte 3/lab#2/RadixSortBinarios.py
+++ Corte 3/lab#2/RadixSortBinarios.py
@@ -1,7 +1,7 @@
-arr=[345, 721, 425, 572, 836, 467, 672,194, 365, 236, 891, 746, 431, 834, 247, 529, 216, 389]
+lista_num=[345, 721, 425, 572, 836, 467, 672,194, 365, 236, 891, 746, 431, 834, 247, 529, 216, 389]
 
-def radix_sort(arr):
-    max_val = max(arr)
+def radix_sort(lista_num):
+    max_val = max(lista_num)
     exp = 1
 
     while max_val // exp > 0:
@@ -9,18 +9,18 @@ def radix_sort(arr):
         buckets = [[] for _ in range(10)]
 
         # Distribuir cada número en su bucket según el dígito actual
-        for num in arr:
+        for num in lista_num:
             digito = (num // exp) % 10
             buckets[digito].append(num)
 
         # Recoger los buckets en orden
-        arr = []
+        lista_num = []
         for bucket in buckets:
-            arr.extend(bucket)
+            lista_num.extend(bucket)
 
         exp *= 10  # siguiente dígito
 
-    return arr
+    return lista_num
 
-resultado = radix_sort(arr)
+resultado = radix_sort(lista_num)
 print(resultado)
